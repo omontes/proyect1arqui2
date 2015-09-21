e4e8c3d6fc659b12d2dbc9c655cac29401297aa2
oscar <oscar@ubuntu>
2015-09-21 14:41:22 -0700

    Se creo un ejemplo usando freeimage (una imagen con fondo azul)

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@
 #include <iostream>
 using namespace std;
 
-#define WIDTH 200
-#define HEIGHT 200
+#define WIDTH 512
+#define HEIGHT 512
 #define BPP 32 //Bits per pixel 
 
 /*
@@ -25,21 +25,47 @@ int main(int argc, char** argv) {
     
     // create the bitmap object
     // allocate a 200x200 pixel image, with 32-bit colour
-    FIBITMAP * bitmap = FreeImage_Allocate(WIDTH, HEIGHT, BPP); 
+    
+    int width;
+    int height;
 
-    // create the blue colour
-    RGBQUAD blue;
-    blue.rgbBlue = 255;
+    FREE_IMAGE_FORMAT formato = FreeImage_GetFileType("sample.png", 0);
+    FIBITMAP* bitmap = FreeImage_Load(formato, "sample.png");
+    
+   
+    
+    
+    FIBITMAP* temp = FreeImage_ConvertTo32Bits(bitmap);
+    
+    width = FreeImage_GetWidth(temp);
+    height = FreeImage_GetHeight(temp);
+    FIBITMAP * new_bitmap = FreeImage_Allocate(width, height, BPP); 
+    
+    FreeImage_Unload(bitmap);
+    bitmap = temp;
+    
+    
+    int c = 2.1;
+    int d = 2.1;
+    
 
     for (int i = 0; i < 200; i++) {
-        for (int j = 0; j < 200; j++) {
+        for (int j = 0; j < 100; j++) {
             // draw a blue pixel at (i, j)
-            FreeImage_SetPixelColor(bitmap, i, j, &blue);
+            // x parte real y y parte imaginaria
+            int a = i;
+            int b = j;
+            int inew_bitmap= (a*c - b*d);
+            int jnew_bitmap= (a*d + b*c);
+            
+            RGBQUAD color;
+            FreeImage_GetPixelColor(bitmap,i,j,&color);
+            FreeImage_SetPixelColor(new_bitmap, inew_bitmap, jnew_bitmap, &color);
         }
     }
 
     // save it as output.bmp
-    FreeImage_Save(FIF_BMP, bitmap, "output.bmp");
+    FreeImage_Save(FIF_BMP, new_bitmap, "output.bmp");
 
     // deallocate memory
     FreeImage_Unload(bitmap);
