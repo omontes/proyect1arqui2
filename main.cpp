5fa86a7b1e7a000dd746c757116e410f7e58c06c
oscar <oscar@ubuntu>
2015-09-21 17:09:40 -0700

    Listo el mapeo, falta validar el tamano maximo

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <FreeImage.h>
 #include <cstdlib>
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 #define WIDTH 512
@@ -44,23 +45,39 @@ int main(int argc, char** argv) {
     FreeImage_Unload(bitmap);
     bitmap = temp;
     
+    RGBQUAD negro;
+    negro.rgbBlue = 0;
+    negro.rgbRed = 0;
+    negro.rgbGreen = 0;
     
-    int c = 2.1;
-    int d = 2.1;
-    
-
-    for (int i = 0; i < 200; i++) {
-        for (int j = 0; j < 100; j++) {
+    /*
+     * Mapea 0, tmaano en x j y lo mapea y luego mapea oarte ral en 
+     */
+    /*El tamano de la imagen
+    Despues probar las 4 esquinas, mapeo normal y 
+    entonces saca cual es el valor en la otra imagen, define el bitmap del tamano resultato.*/
+ 
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
             // draw a blue pixel at (i, j)
             // x parte real y y parte imaginaria
-            int a = i;
-            int b = j;
-            int inew_bitmap= (a*c - b*d);
-            int jnew_bitmap= (a*d + b*c);
+            double A = i-(height-j);
+            double B = i+(height-j);
+            double D = 2.1-0.003*i;
+            double E = 2.1-0.003*(height-j);
+            double divisor = pow(D,2)+pow(E,2);
+            int inew_bitmap= ((A*D)+(B*E))/divisor;
+            int jnew_bitmap= ((B*D)-(A*E))/divisor;
             
+            /*Sacar el mapeo de inverso*/
+            
+            if(inew_bitmap<0 | jnew_bitmap<0 | jnew_bitmap>height | inew_bitmap>width ){
+                FreeImage_SetPixelColor(new_bitmap, i, j, &negro);
+            }
+            else{
             RGBQUAD color;
-            FreeImage_GetPixelColor(bitmap,i,j,&color);
-            FreeImage_SetPixelColor(new_bitmap, inew_bitmap, jnew_bitmap, &color);
+            FreeImage_GetPixelColor(bitmap,inew_bitmap,(height-jnew_bitmap),&color);
+            FreeImage_SetPixelColor(new_bitmap, i, j, &color);}
         }
     }
 
